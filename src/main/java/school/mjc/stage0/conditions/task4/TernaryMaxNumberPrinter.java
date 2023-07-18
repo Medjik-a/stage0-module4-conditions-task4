fcaff1f927720a2d2caec5c9ca0e27c78bba84eb
medjik <med.akhmetov@gmail.com>
2023-07-18 17:35:26 +0600

    first attempt

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int result = first>second?first:second;
+        System.out.println(third>result?third:result);
     }
 }
